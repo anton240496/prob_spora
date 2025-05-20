404cc1575ef99aaef740eb7689c2e23698e4e3de
anton240496 <47662779+anton240496@users.noreply.github.com>
2025-05-20 22:11:07 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+      $('.slider').slick({
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick_arrow slick_prev"><img src="/wp-content/themes/prob/assets/img/arrow_prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick_arrow slick_next"><img src="/wp-content/themes/prob/assets/img/arrow_next.svg" alt=""></button>',
+    });
+
+});
